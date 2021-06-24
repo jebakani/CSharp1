0b2b704b0c0785cb2b2e27763cb94bd5586ab0a2
Jebakani <vjishwarya@gmail.com>
2021-06-24 15:52:17 +0530

    initial commit

---
+++ Calculator/BestCodingPractices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class BestCodingPractices
+    {
+        string myName = "Xyz";
+
+        //Adding addition method
+        public int Addition(int number1,int number2)
+        {
+            return number1 + number2;
+
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            BestCodingPractices bestCoding = new BestCodingPractices();
+            Console.WriteLine(""+bestCoding.Addition(5,7));
+        }
+    }
+}
